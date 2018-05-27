0be4d7708665040335ee7affdc67ec4ee8c899ca
RomanMozolevskij <rmrmdecoy@gmail.com>
2018-05-27 14:12:30 +0300

    default controller changes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,30 +2,16 @@
 
 namespace App\Controller;
 
-
-use App\Entity\Skills;
-use App\Service\SerializeService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class DefaultController extends Controller
 {
-    private $serializeService;
-
-    public function __construct(SerializeService $serializeService)
-    {
-
-        $this->serializeService = $serializeService;
-    }
-
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-        $data = $this->serializeService->serialize(Skills::class);
-        return new Response($data);
+        return $this->render('index.html.twig');
     }
 }
