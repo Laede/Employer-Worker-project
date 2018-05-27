98c938b6d05430437b60351a0a82853d7ae935b3
Darius <drx125@gmail.com>
2018-05-27 14:52:18 +0300

    filters actions moved to service

--- src/Service/WorkerService.php
+++ src/Service/WorkerService.php
@@ -6,6 +6,7 @@ namespace App\Service;
 use App\Entity\User;
 use App\Entity\Worker;
 use App\Repository\WorkerRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class WorkerService
 {
@@ -26,5 +27,26 @@ class WorkerService
         $worker = $this->workerRepository->findOneBy(['user' => $user]);
 
         return $worker && $worker->getCv();
+    }
+
+    public function proceedFilters(Request $request, User $user)
+    {
+        $filter2method = [
+            'skills'        => 'filterSkills',
+            'my_skills'     => 'filterSkills',
+            'budget_from'   => 'filterBudgetFrom',
+            'budget_to'     => 'filterBudgetTo'
+        ];
+        $filters = [];
+        foreach($request->query as $key => $item) {
+            if($item && isset($filter2method[$key])) {
+                $filters[$key] = [
+                    'filter'    => $key,
+                    'method'    => $filter2method[$key],
+                    'value'     => $key === 'my_skills'?$this->getWorker($user)->getSkills():$item,
+                ];
+            }
+        }
+        return $filters;
     }
 }
